03172c702e55ccb016665323f14f78de8e3b9161
Chris Wu <wakeup0501@gmail.com>
2021-07-27 22:44:00 +0800

    add flitering function

--- read.py
+++ read.py
@@ -4,5 +4,9 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		sum_len += len(line)
 		data.append(line)
-print("總共有", len(data), "筆資料")
-print("資料中留言的平均長度為", sum_len/len(data), "個字")
+print("總共有", len(data), "筆留言")
+print("留言的平均長度為", sum_len/len(data), "個字")
+new = [n for n in data if len(n) < 100]
+print("總共有", len(new), "筆留言少於100個字")
+good = [g for g in data if 'good' in g]
+print("總共有", len(good), "筆留言提到good")
